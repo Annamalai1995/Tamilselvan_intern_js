ce9ed7fabc9a0fac3166c4f1d57d097898d41397
Annamalai1995 <s.annamalai1995@gmail.com>
2024-11-28 13:20:01 +0530

    function and arrays

---
+++ Function.js
@@ -0,0 +1,19 @@
+function salem()   //without parameter
+{  
+    alert("THIs Is Normal Function and using alert method")
+}
+function chennai(beach,resort)    //with parameter
+{
+
+    document.writeln("BEACH NAME IS:"+beach+"RESORT NAME:"+resort);
+}
+
+let student=()=>
+{
+    alert("THIS IS FAT ARROW FUNCTION WITH ALERT METHOD")
+
+}
+var Sum=(cosmo,alpha)=>
+{
+    return cosmo*alpha;
+}
